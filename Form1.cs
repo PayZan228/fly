058019455ccd4224f310eb76d12fd93d05af5b38
Денис Егорычев <den.egorychev@hotmail.com>
2018-12-20 15:58:21 +0300

    First commit

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            for (int i = 0; i < 11; i++)
+            {
+                int x;
+                x = pictureBox1.Width / 11;
+                x *= i;
+                //y = pictureBox1.Height / 15;
+                //y *= j;
+
+                Point p1 = new Point(x, 0);
+                Point p2 = new Point(x, pictureBox1.Height);
+                Pen ruch = new Pen(Color.Aqua);
+                g.DrawLine(ruch, p1, p2);
+            }
+            for (int i = 0; i < 11; i++)
+            {
+                int y;
+                y = pictureBox1.Height / 11;
+                y *= i;
+                Point p3 = new Point(0, y);
+                Point p4 = new Point(pictureBox1.Width, y);
+                Pen ruch2 = new Pen(Color.Aqua);
+                g.DrawLine(ruch2, p3, p4);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp7/WindowsFormsApp7/Classes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  WindowsFormsApp7
+{
+    class Pulya
+    {
+        public int damage;
+        public Pulya()
+        {
+            damage = 50;
+        }
+    }
+    class Plane
+    {
+        public int hp;
+        private int magazine;
+        public Plane()
+        {
+            hp = 1000;
+            magazine = 500;
+        }
+        public Pulya GetPulya()
+        {
+            Pulya bullet = new Pulya();
+            magazine--;
+            return bullet;
+        }
+        public void takedamage(int damaga)
+        {
+            hp -= damaga;
+        }
+    }
+
+    class YA
+    {
+        public int type;
+        public YA(int yach)
+        {
+            type = yach;
+        }
+    }
+    class pole
+    {
+        YA[,] polee;
+        public pole()
+        {
+            polee = new YA[11, 15];
+            for (int i = 0; i < 11; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    polee[i, j] = new YA(1);
+                }
+
+            }
+
+            polee[6, 10].type = 3;
+        }
+
+        public void Tick()
+        {
+            for (int i = 14; i > 0; i--)
+            {
+                for (int j = 0; j < 11; j++)
+                {
+                    polee[i + 1, j] = polee[i, j];
+                }
+            }
+        }
+
+
+    }
+}
